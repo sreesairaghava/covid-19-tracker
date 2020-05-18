79aebcb7a2c9ce9782433777cb2895b7906be6bd
Sree Sai Raghava <raghava.rym@gmail.com>
2020-05-18 11:32:35 +0530

    Making more generic

--- frontend/src/components/VisualBlock.js
+++ frontend/src/components/VisualBlock.js
@@ -4,13 +4,16 @@ import CanvasJSReact from "../lib/canvasJs/canvasjs.react";
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 var dataPoints = [];
 class VisualBlock extends Component {
+  state = {
+    stateNames: [],
+  };
   render() {
     const options = {
       animantionEnabled: true,
       exportEnabled: true,
       theme: "light2",
       title: {
-        text: "CoronaVirus Tracker Andhra Pradesh",
+        text: `Coronavirus Tracker ${this.state.stateNames[20]}`,
       },
       axisX: {
         title: "Days",
@@ -46,19 +49,27 @@ class VisualBlock extends Component {
       })
       .then((data) => {
         var stateData = [];
+        var stateNames = [];
         for (var i in data) {
           stateData.push(data[i]);
         }
-        var tamilNadu = stateData.filter((item) => {
-          return item["State UT"] === "Andhra Pradesh";
+        for (var j in data) {
+          stateNames.push(Object.values(data[j]).pop());
+        }
+        this.setState(() => ({
+          stateNames: stateNames,
+        }));
+        console.log(this.state.stateNames);
+        var singleState = stateData.filter((item) => {
+          return item["State UT"] === `${stateNames[20]}`;
         });
-        console.log(tamilNadu[0]);
-        return tamilNadu;
+        console.log(singleState[0]);
+        return singleState;
       })
-      .then((tamilNadu) => {
-        var keys_ext = Object.keys(tamilNadu[0]).slice(0, -1);
+      .then((singleState) => {
+        var keys_ext = Object.keys(singleState[0]).slice(0, -1);
         var final_keys = keys_ext;
-        var values_ext = Object.values(tamilNadu[0]).slice(0, -1);
+        var values_ext = Object.values(singleState[0]).slice(0, -1);
         var final_val = values_ext;
         for (var i = 0; i < final_keys.length; i++) {
           dataPoints.push({
